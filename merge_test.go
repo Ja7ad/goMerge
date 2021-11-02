f128e4a0806df73a01874530440338ad13d90062
Javad <ja7ad@live.com>
2021-11-02 12:56:20 +0330

    Add unit test for Merge function

---
+++ merge_test.go
@@ -0,0 +1,28 @@
+package fileMerger
+
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+)
+
+const numOfFile = 5
+
+func Test_Merge(t *testing.T) {
+	done := make(chan struct{})
+	defer close(done)
+
+	// create file with random data
+	for i := 0; i < numOfFile; i++ {
+		buf := make([]byte, 10000)
+		err := ioutil.WriteFile(fmt.Sprintf("./test/test-%d.txt", i), buf, 0666)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	err := Merge("./test/", ".txt", "./test/merged.txt", true)
+	if err != nil {
+		t.Error(err)
+	}
+}
